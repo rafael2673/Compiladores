cdd57f3bad67c3751fe3d6a1cc1e569988f86447
Rafael Franco <rafaelribeirofranco4@gmail.com>
2024-11-11 20:03:13 -0300

    alterações finais

--- Lab04/parser.cpp
+++ Lab04/parser.cpp
@@ -7,15 +7,53 @@ void Parser::Start()
     while ((lookahead = scanner.yylex()) != 0)
     {
         // trata o token recebido do analisador léxico
-        switch(lookahead)
+        switch (lookahead)
         {
-            case WHILE: cout << "WHILE\n"; break;
-            case IF: cout << "IF\n"; break;
-            case THEN: cout << "THEN\n"; break;
-            case ELSE: cout << "ELSE\n"; break;
-            case ID: cout << "ID: " << scanner.YYText() << "\n"; break;
-            case NUM: cout << "NUM: " << scanner.YYText() << "\n"; break;
-            case RELOP: cout << "RELOP: " << scanner.YYText() << "\n";; break;
+        case WHILE:
+            cout << "WHILE\n";
+            break;
+        case IF:
+            cout << "IF\n";
+            break;
+        case THEN:
+            cout << "THEN\n";
+            break;
+        case ELSE:
+            cout << "ELSE\n";
+            break;
+        case ID:
+            cout << "ID: " << scanner.YYText() << "\n";
+            break;
+        case NUM:
+            cout << "NUM: " << scanner.YYText() << "\n";
+            break;
+        case STRING:
+        {
+            std::string str = scanner.YYText(); // obtém a string reconhecida
+            // Remove as aspas iniciais e finais
+            str = str.substr(1, str.size() - 2);
+
+            // Substitui sequências de escape
+            size_t pos = 0;
+            while ((pos = str.find("\\\"", pos)) != std::string::npos)
+            {
+                str.replace(pos, 2, "\"");
+                pos += 1;
+            }
+
+            pos = 0;
+            while ((pos = str.find("\\\\", pos)) != std::string::npos)
+            {
+                str.replace(pos, 2, "\\");
+                pos += 1;
+            }
+
+            cout << "STRING: " << str << "\n";
+            break;
+        }
+        case RELOP:
+            cout << "RELOP: " << scanner.YYText() << "\n";
+            break;
         }
     }
 }
